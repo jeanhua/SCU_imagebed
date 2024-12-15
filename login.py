17d253a92724e8e49b0bf0a504af99c82d1578ef
jeanhua <2207739460@qq.com>
2024-12-15 17:40:36 +0800

    add ocr

--- login.py
+++ login.py
@@ -1,8 +1,9 @@
+import base64
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+import ddddocr
 
 
 def login(id, password):
@@ -13,15 +14,19 @@ def login(id, password):
     user_input = drivers.find_element(by=By.XPATH, value='//*[@id="app"]/div[1]/div/div[2]/div/div[1]/div[2]/div[2]/div/form/div[1]/div/div/div[2]/div/input')
     pw_input = drivers.find_element(by=By.XPATH, value='//*[@id="app"]/div[1]/div/div[2]/div/div[1]/div[2]/div[2]/div/form/div[2]/div/div/div[2]/div/input')
 
-    # 自动识别验证码,懒得搞了
+    # 自动识别验证码
     vercode_input = drivers.find_element(by=By.XPATH,value='//*[@id="app"]/div[1]/div/div[2]/div/div[1]/div[2]/div[2]/div/form/div[3]/div/div/div/div/input')
     vercode = drivers.find_element(by=By.XPATH,value='//*[@id="app"]/div[1]/div/div[2]/div/div[1]/div[2]/div[2]/div/form/div[3]/div/div/img')
+    vercode_base64 = vercode.get_attribute("src").split(',')[1]
+    ocr = ddddocr.DdddOcr(show_ad=False)
+    res = ocr.classification(base64.b64decode(vercode_base64))
+    vercode_input.send_keys(res)
 
     login_btn = drivers.find_element(by=By.XPATH, value='//*[@id="app"]/div[1]/div/div[2]/div/div[1]/div[2]/div[2]/div/form/div[4]/div/button')
     user_input.send_keys(id)
     pw_input.send_keys(password)
-    # time.sleep(0.8)
-    # login_btn.click()
+    time.sleep(0.8)
+    login_btn.click()
     WebDriverWait(drivers, 120, 0.5).until(lambda
                                                driver: driver.current_url == "https://ecourse.scu.edu.cn/learn/live?type=0",
                                            "请检查网络连接")
--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-import sys
+import os.path
 import postImage
 import login
-import subprocess
-import importlib.util
-
-def install_and_import(package):
-    try:
-        # 尝试导入库
-        importlib.util.find_spec(package)
-        return importlib.import_module(package)
-    except ImportError:
-        # 如果导入失败，尝试使用pip安装
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-        # 安装后再次尝试导入
-        return importlib.import_module(package)
 
 if __name__ == "__main__":
-    requests = install_and_import('requests')
-    selenium = install_and_import('selenium')
     print('四川大学统一身份登陆')
     id = input("输入学号：")
     password = input("输入密码：")
@@ -30,11 +15,14 @@ if __name__ == "__main__":
         print("输入操作：")
         print("1.查看所有图片")
         print("2.上传图片")
-        print("3.退出\n")
+        print("3.退出")
         op = input()
         if op == "1":
-            with open("./image.txt", 'r', encoding='utf-8') as f:
-                print(f.read())
+            if os.path.exists("./image.txt"):
+                with open("./image.txt", 'r', encoding='utf-8') as f:
+                    print(f.read())
+            else:
+                print("没有什么东西……")
         if op == "2":
             filepath = input("输入图片路径：").replace('"','')
             back = postImage.PostImage(filepath, cookie).run()
