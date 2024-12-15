f495fa759ab7c267e545ac850adb1bb347617e14
jeanhua <2207739460@qq.com>
2024-12-15 21:48:37 +0800

    readme

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     id = input("输入学号：")
     password = input("输入密码：")
     cookie = login.login(id, password)
-    if cookie==None:
+    if cookie == None:
         print('登陆失败')
         exit()
 
@@ -24,14 +24,14 @@ if __name__ == "__main__":
             else:
                 print("没有什么东西……")
         if op == "2":
-            filepath = input("输入图片路径：").replace('"','')
+            filepath = input("输入图片路径：").replace('"', '')
             back = postImage.PostImage(filepath, cookie).run()
             if back == None:
                 print("上传失败！")
                 continue
-            print("上传成功！URL:"+back.replace(' ','%20'))
+            print("上传成功！URL:" + back.replace(' ', '%20'))
             with open("./image.txt", 'a', encoding="utf-8") as f:
-                f.write(back.replace(' ','%20') + '\n')
+                f.write(back.replace(' ', '%20') + '\n')
         if op == '3':
             break
         else:
